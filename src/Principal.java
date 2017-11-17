615e9f855c077d48ad85ed5db6091bd6d5cf6c18
admin <admin@OSMARVM0>
2017-11-16 23:17:44 -0200

    Correção dos índices do vetor e comentários

--- src/Principal.java
+++ src/Principal.java
@@ -4,12 +4,20 @@
  * Programa de Pós-Graduação em Ciências da Computação - PROPG
  * Disciplinas: Projeto e Análise de Algoritmos
  * Prof Alexandre Gonçalves da Silva 
+ *
  * Baseado nos slides 24 da aula do dia 22/09/2017  
+ *
  * Página 156 Cormen 3a Ed
  *
  * Problema de Seleção
  * Dado um conjunto A de n números inteiro e um inteiro i, 
  * determinar o i-ésimo menor elemento de A.
+ *
+ * Atenção:
+ * Vetor em java inicia em 0, os algoritmos consideram início em 1.
+ * A subtraçào de -1 ocorre somente no local de acesso ao vetor ou matriz 
+ * para manter a compatibilidade entre os algoritmos.
+ *
  */
 
 /**
@@ -30,40 +38,40 @@ public class Principal {
     public static int[] minMax(int A[], int n) {        
         int min;
         int max;
-        int i = 0;
+        int i = 1;
         //Verifica se n é impar
         if (n%2==1){              
-            min = A[i];
-            max = A[i];            
+            min = A[i-1];
+            max = A[i-1];            
             //Avança um elemento pois temos quantidade impar de elementos
             i = i + 1;
         } else {
             //Compara os dois primeiros para saber que é o menor e maior
-            if (A[i] > A[i+1]){
-                min = A[i+1];
-                max = A[i];
+            if (A[i-1] > A[i+1-1]){
+                min = A[i+1-1];
+                max = A[i-1];
             } else {
-                min = A[i];
-                max = A[i+1];
+                min = A[i-1];
+                max = A[i+1-1];
             }
             //Pula dois elementos pois temos quantidade par de elementos
             i = i + 2;
         }
         
-        while (i < n){           
-            if (A[i]> A[i+1]){ 
-                if (A[i]>max){
-                    max = A[i];                    
+        while (i <= n){           
+            if (A[i-1]> A[i+1-1]){ 
+                if (A[i-1]>max){
+                    max = A[i-1];                    
                 }
-                if (A[i+1]<min){
-                    min = A[i+1];  
+                if (A[i+1-1]<min){
+                    min = A[i+1-1];  
                 };
             } else {                
-                if (A[i]<min){
-                   min = A[i];                   
+                if (A[i-1]<min){
+                   min = A[i-1];                   
                 }             
-                if (A[i+1]>max){
-                   max = A[i+1];  
+                if (A[i+1-1]>max){
+                   max = A[i+1-1];  
                 }
             }
             //Avança mais dois elementos
